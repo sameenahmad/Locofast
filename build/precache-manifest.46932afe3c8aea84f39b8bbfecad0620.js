97a4e062608b6fcebbc923394970c983242d7f34
Sameen <sameen.ahmad21@gmail.com>
2019-07-12 14:22:16 +0530

    Deploying on Github pages

---
+++ build/precache-manifest.46932afe3c8aea84f39b8bbfecad0620.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5492c175a917434c65be7edede3c0ab0",
+    "url": "/Locofast/index.html"
+  },
+  {
+    "revision": "f89744cbeb311c6d83fe",
+    "url": "/Locofast/static/css/main.b8d9e34e.chunk.css"
+  },
+  {
+    "revision": "10cc360c574156459d91",
+    "url": "/Locofast/static/js/2.76a6f6ae.chunk.js"
+  },
+  {
+    "revision": "f89744cbeb311c6d83fe",
+    "url": "/Locofast/static/js/main.baf01742.chunk.js"
+  },
+  {
+    "revision": "8fb1dba3951869bb90e2",
+    "url": "/Locofast/static/js/runtime~main.9031862f.js"
+  },
+  {
+    "revision": "0ccc3389e6fa76259962fac9e56ec3ab",
+    "url": "/Locofast/static/media/account.0ccc3389.svg"
+  },
+  {
+    "revision": "b5ce6eb5982e1553124d2ff9f6d6dd28",
+    "url": "/Locofast/static/media/add.b5ce6eb5.svg"
+  },
+  {
+    "revision": "1f6abe652fb699a8c31da390c99de2f9",
+    "url": "/Locofast/static/media/arrow.1f6abe65.svg"
+  },
+  {
+    "revision": "a76271e7c8c9245dcc48f83789a372b2",
+    "url": "/Locofast/static/media/collections.a76271e7.svg"
+  },
+  {
+    "revision": "e9eed4c5affcdc3c17a26a1505b1c86f",
+    "url": "/Locofast/static/media/messages.e9eed4c5.svg"
+  },
+  {
+    "revision": "a06833fdc118fc5b40bec6d821135333",
+    "url": "/Locofast/static/media/production.a06833fd.svg"
+  },
+  {
+    "revision": "4911729d6c194584429b0db82b3a7ef2",
+    "url": "/Locofast/static/media/sampling.4911729d.svg"
+  },
+  {
+    "revision": "33a31a95176f274d7977eb0f59ca78f3",
+    "url": "/Locofast/static/media/search.33a31a95.svg"
+  }
+]);
